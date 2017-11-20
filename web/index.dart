23cd18d01be5c7e8895fbcdb1bbe961e3db217d0
Joel Leibow <joel.leibow@workiva.com>
2017-11-19 22:05:52 -0700

    Handle custom element registration at startup

--- web/index.dart
+++ web/index.dart
@@ -4,5 +4,4 @@ import 'package:ss_experience/ss_experience.dart';
 
 void main() {
   document.registerElement('ss-experience', SSExperience);
-  document.querySelector('#ssExperience').append(new Element.tag('ss-experience'));
 }
